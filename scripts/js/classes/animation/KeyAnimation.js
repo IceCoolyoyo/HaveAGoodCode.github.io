992c4450637ce3f747cd7b4b9feb27762c1e3410
NOBTG <nobtg987@gmail.com>
2024-12-15 00:01:03 +0900

    fixed typing animation

--- scripts/js/classes/animation/KeyAnimation.js
+++ scripts/js/classes/animation/KeyAnimation.js
@@ -1,4 +1,3 @@
-import DebugTool from '../debug/DebugTool.js';
 class KeyAnimation {
     static get canCountinue() {
         return KeyAnimation.countinue;
@@ -7,23 +6,40 @@ class KeyAnimation {
         KeyAnimation.countinue = !KeyAnimation.countinue;
     }
     static setObjAnimation(string, obj, runnable) {
-        const width = KeyAnimation.calcWidth(string);
         KeyAnimation.toggleCountinue();
-        obj.innerHTML = string;
-        obj.style.width = `${width}ch`;
         obj.style.borderRightColor = 'rgb(0, 0, 0)';
-        obj.style.animation = `typing ${width / 10}s steps(${string.length}), caret 0.8s steps(1) infinite`;
-        setTimeout(() => {
-            KeyAnimation.clearObjAnimation(obj);
+        obj.style.animation = `caret 0.8s steps(1) infinite`;
+        KeyAnimation.typing(string, obj, 110, () => setTimeout(() => {
+            obj.style.borderRightColor = 'transparent';
             KeyAnimation.toggleCountinue();
             const div = document.createElement("div");
             div.id = "question-title";
             div.innerText = string;
             document.getElementById("left").appendChild(div);
-            if (runnable) {
+            if (runnable !== undefined && runnable !== null) {
                 runnable();
             }
-        }, ((width / 10) * 1000) + 500);
+        }, 500));
+    }
+    static typing(string, element, typingInterval, endRun, currentIndex = 0, isInitialCall = true) {
+        if (isInitialCall) {
+            element.textContent = "";
+            setTimeout(() => {
+                KeyAnimation.typing(string, element, typingInterval, endRun, currentIndex, false);
+            }, typingInterval);
+            return;
+        }
+        if (currentIndex >= string.length) {
+            endRun();
+            return;
+        }
+        const currentChar = string[currentIndex];
+        element.textContent += currentChar;
+        currentIndex++;
+        const delay = currentChar === " " ? 0 : typingInterval;
+        setTimeout(() => {
+            KeyAnimation.typing(string, element, typingInterval, endRun, currentIndex, false);
+        }, delay);
     }
     static setObjAnimation2(obj, callback) {
         KeyAnimation.toggleCountinue();
@@ -32,29 +48,6 @@ class KeyAnimation {
             KeyAnimation.toggleCountinue();
             callback === null || callback === void 0 ? void 0 : callback();
         }, 100);
-    }
-    static clearObjAnimation(obj) {
-        obj.style.borderRightColor = 'transparent';
-        obj.style.animation = ``;
-    }
-    static deTypingAnimation(width, obj) {
-        obj.style.animation = `deTyping ${width / 10}s steps(${obj.innerHTML.length}), caret 0.8s steps(1) infinite`;
-        setTimeout(() => {
-            KeyAnimation.clearObjAnimation(obj);
-        }, ((width / 10) * 1000));
-    }
-    static calcWidth(string) {
-        const chineseCharRegex = /[\u4E00-\u9FFF]/;
-        const doubleWidthCharRegex = /[\u3000-\u303F\uFF00-\uFFFF]/;
-        const debugArray = [];
-        let width = 0;
-        for (let char of string) {
-            const charWidth = chineseCharRegex.test(char) || doubleWidthCharRegex.test(char) ? 2 : 1;
-            width += charWidth;
-            DebugTool.ifDebug(() => debugArray.push({ char, width: charWidth }));
-        }
-        DebugTool.ifDebug(() => console.debug("%cFunction %ccalcWidth", "color: #CCEEFF;", "color: #FFC8B4; font-weight: bold;", debugArray));
-        return width;
     }
 }
 KeyAnimation.countinue = true;
