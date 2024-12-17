9f27e7e2446bddb579fb542f2a31e1ec4311b477
HaveAGoodCode <nobtg987@gmail.com>
2024-12-17 19:11:59 +0800

    fixed js

--- scripts/js/Codes.js
+++ scripts/js/Codes.js
@@ -6,6 +6,8 @@ Codes.helloWorld = `
                 System.out.println("Hello World!");
             }
         }`;
+Codes.q0 = `
+    System.out.println(666);`;
 Codes.q1 = `
     true or false?`;
 Codes.q2 = `
@@ -31,12 +33,42 @@ Codes.q6 = `
     true or false?`;
 Codes.q7 = `
     System.out.println(2+(6+4)/2);`;
-Codes.q8 = `
+Codes.q8_1 = `
+    System.out.println(561-999<999-666);`;
+Codes.q8_2 = `
     System.out.println(8+4849*9>=32*94+96*12);`;
 Codes.q9 = `
     boolean x = true;
     x = !x;
     System.out.println(!x);`;
+Codes.q10 = `
+    boolean x = false;
+    boolean y = true;
+    System.out.println(x || y);`;
+Codes.q11 = `
+    boolean x = false;
+    boolean y = true;
+    System.out.println(x && y);`;
+Codes.q12 = `
+    int i = 1;
+    System.out.println(i);`;
+Codes.q13 = `
+    long i = 5L;
+    System.out.println(i);`;
+Codes.q14 = `
+    float i = 5.15156156456156F;
+    System.out.println(i);`;
+Codes.q15 = `
+    float i = 456456456.1544454545;
+    System.out.println(i);`;
+Codes.q16 = `
+    int i = 5; i = i + 6; System.out.println(i);`;
+Codes.q17 = `
+    int i = 9; i = i - 6; System.out.println(i);`;
+Codes.q18 = `
+    int i = 3; i = i * 6; System.out.println(i);`;
+Codes.q19 = `
+    int i = 42; i = i / 6; System.out.println(i);`;
 Codes.println = `
     System.out.println("Hello World!");`;
 Codes.equalTrue = `
@@ -53,7 +85,8 @@ Codes.redeclareBool = `
 Codes.do2Things = `
     boolean isSunny = true;`;
 Codes.do2ThingsReDecBool = `
-    isSunny = true; isSunny = false;`;
+    isSunny = true;
+    isSunny = false;`;
 Codes.notEqualTrue = `
     System.out.println(5 != 3);`;
 Codes.notEqualFalse = `
@@ -70,4 +103,18 @@ Codes.orGateFF = `
     System.out.println(false || false);`;
 Codes.orGateTT = `
     System.out.println(true || true);`;
+Codes.andGateTF = `
+    System.out.println(true && true);`;
+Codes.andGateFF = `
+    System.out.println(false && false);`;
+Codes.andGateTT = `
+    System.out.println(true && true);`;
+Codes.add = `
+    int a = 0; a = a + 1; System.out.println(a);`;
+Codes.minus = `
+    int a = 0; a = a - 1; System.out.println(a);`;
+Codes.multiply = `
+    int a = 2; a = a * 2; System.out.println(a);`;
+Codes.divide = `
+    int a = 6; a = a / 3; System.out.println(a);`;
 export default Codes;
--- scripts/js/Drama.js
+++ scripts/js/Drama.js
@@ -6,25 +6,24 @@ Part.Part1 = Object.freeze(`@Ball:true為真，false為假
         @Ball:如果今天是星期一，電腦就會回答 true；
         @Ball:如果不是，電腦就會回答 false。
         @Ball:「水有毒嗎?」
-        @Code:helloWorld
+        @Code:q1
         @Answer:false`);
 Part.Part2 = Object.freeze(`@Ball:==用來判斷是否相等
-        @Ball:跟電腦說5 == 5，電腦會回答true(真的)
+        @Ball:如果跟電腦說5 == 5，那電腦就會回答true(真的)
         @Ball:因為 5 跟 5 是一樣的。
-        @Ball:但如果跟電腦說5 == 3
-        @Ball:那電腦就會回答 false (假的)
+        @Ball:但如果跟電腦說5 == 3，那電腦就會回答false(假的)
         @Ball:因為 5 跟 3 不一樣。
-        @Answer:false`);
+        @Ball:「8==8，試問電腦會回答什麼?」
+        @Answer:true`);
 Part.Part3 = Object.freeze(`@Code:println
-        @Ball:用來輸出結果
-        @Ball:就是讓電腦把東西「寫出來」。
-        @Answer:false`);
+        @Ball:用來輸出結果，就是讓電腦把東西「寫出來」。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q0
+        @Answer:666`);
 Part.Part4 = Object.freeze(`@Code:equalTrue
-        @Ball:這樣電腦會輸出 true
-        @Ball:因為 5 跟 5 是一樣的。
+        @Ball:這樣電腦會輸出true，因為5跟5是一樣的。
         @Code:equalFalse
-        @Ball:那電腦會輸出 false
-        @Ball:因為 5 跟 3 不一樣。
+        @Ball:那電腦會輸出false，因為5跟3不一樣。
         @Ball:「123和321是否相同?」
         @Code:q2
         @Answer:false`);
@@ -32,11 +31,14 @@ Part.Part5 = Object.freeze(`@Ball:電腦需要儲存資料時
         @Ball:電腦需要知道資料的長相才能儲存
         @Ball:所以true和false其實是一種特定的「資料長相」
         @Ball:叫做 boolean。
-        @Ball:意思是電腦會用這個 「boolean」 
+        @Ball:「true和false是什麼?」
+        @Answer:boolean`);
+Part.owo1 = Object.freeze(`@Ball:意思是電腦會用這個 「boolean」 
         @Ball:來儲存只有「真」或「假」的資訊
-        @Ball:就像電腦在說：「喔，這個東西是 boolean
-        @Ball:所以它只能是 true 或 false。」
-        @Answer:false`);
+        @Ball:就像電腦在說：
+        @Ball:「喔，這個東西是 boolean，所以它只能是 true 或 false。」
+        @Ball:「boolean只能是?」
+        @Answer:true或false`);
 Part.Part6 = Object.freeze(`@Ball:我們還可以用「名字 = 東西;」
         @Ball:來把資料放進剛剛建立的空間裡。
         @Code:declareBool
@@ -47,10 +49,8 @@ Part.Part6 = Object.freeze(`@Ball:我們還可以用「名字 = 東西;」
         @Answer:false`);
 Part.Part7 = Object.freeze(`@Ball:而且名字還可以一直用
         @Code:redeclareBool
-        @Ball:把裡面的資料換成 false
-        @Ball:可能原本天氣是晴朗的
-        @Ball:但後來天氣變陰了
-        @Ball:就可以告訴電腦後來天氣的變化。
+        @Ball:把裡面的資料換成false，可能原本天氣是晴朗的
+        @Ball:但後來天氣變陰了，就可以告訴電腦後來天氣的變化。
         @Ball:「isRainy裡面存的是什麼?」
         @Code:q4
         @Answer:true`);
@@ -59,7 +59,9 @@ Part.Part8 = Object.freeze(`@Ball:我們還可以一次完成兩件事：
         @Ball:用「長相 名字 = 資料;」的方式
         @Ball:電腦就會自動完成這兩件事。
         @Code:do2Things
-        @Ball:這樣就直接建立了一個 isSunny 的空間
+        @Ball:「可以使用什麼格式/方式讓電腦自動完成『建立空間，然後把資料存進去』?」
+        @Answer:長相 名字 = 資料;`);
+Part.owo2 = Object.freeze(`@Ball:這樣就直接建立了一個 isSunny 的空間
         @Ball:並且存了 true表示天氣是晴天。
         @Ball:同樣的，如果之後天氣變了
         @Code:do2ThingsReDecBool
@@ -70,11 +72,9 @@ Part.Part8 = Object.freeze(`@Ball:我們還可以一次完成兩件事：
         @Answer:true`);
 Part.Part9 = Object.freeze(`@Ball:!= 就是用來判斷「兩個東西是不是不同」的。
         @Code:notEqualTrue
-        @Ball:這樣就會輸出 true
-        @Ball:因為 5 和 3 不一樣。
+        @Ball:這樣就會輸出 true，因為 5 和 3 不一樣。
         @Code:notEqualFalse
-        @Ball:那它會輸出 false
-        @Ball:因為 5 跟 5 是一樣的。
+        @Ball:那它會輸出 false，因為 5 跟 5 是一樣的。
         @Ball:「請回答下列程式碼的輸出?」
         @Code:q6
         @Answer:false`);
@@ -84,7 +84,10 @@ Part.Part10 = Object.freeze(`@Ball:電腦的加減乘除，基本上跟數學一
         @Ball:減法：2 - 1，電腦回答 1、
         @Ball:乘法：2 * 1，就是數學的2 × 1，電腦回答 2、
         @Ball:除法：4 / 2，就是數學的4 ÷ 2，電腦回答 2。
-        @Ball:括號也和數學裡一樣。
+        @Ball:「電腦的加減乘除是否和數學一樣?」
+        @Code:q1
+        @Answer:true`);
+Part.owo3 = Object.freeze(`@Ball:括號也和數學裡一樣。
         @Ball:所以如果想改變計算的順序
         @Ball:就可以用括號把它包起來。
         @Ball:比如說：(3 + 5) * 2這樣電腦會先算括號裡的 3 + 5 = 8
@@ -97,11 +100,12 @@ Part.Part10 = Object.freeze(`@Ball:電腦的加減乘除，基本上跟數學一
         @Answer:7`);
 Part.Part11 = Object.freeze(`@Ball:電腦連大於、小於、大於或等於、小於或等於
         @Ball:這些數學符號也能用，而且和數學完全一樣。
-        @Ball:比如： 大於：5 > 3
-        @Ball:電腦會回答 true因為 5 比 3 大、
-        @Ball:小於：3 < 5
-        @Ball:電腦會回答 true，因為 3 比 5 小、
-        @Ball:大於或等於：5 >= 5
+        @Ball:大於：5 > 3電腦會回答 true，因為5比3大、
+        @Ball:小於：3 < 5電腦會回答 true，因為3比5小、
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q8_1
+        @Answer:false`);
+Part.owo4 = Object.freeze(`@Ball:大於或等於：5 >= 5
         @Ball:電腦會回答 true，因為 5 等於 5、
         @Ball:小於或等於：3 <= 5
         @Ball:電腦會回答 true，因為 3 比 5 小。
@@ -109,7 +113,7 @@ Part.Part11 = Object.freeze(`@Ball:電腦連大於、小於、大於或等於、
         @Ball:這樣就會輸出 false
         @Ball:因為 7 既不比 8 大，也不等於 8。
         @Ball:「請回答下列程式碼的輸出?」
-        @Code:q8
+        @Code:q8_1
         @Answer:true`);
 Part.Part12 = Object.freeze(`@Ball:! 是用來反轉 boolean 值的
         @Ball:這就像告訴電腦：「如果它是真的，給我假的；
@@ -131,45 +135,68 @@ Part.Part13 = Object.freeze(`@Ball:|| 是「或」的意思
         @Ball:這次兩個條件都是假的，所以結果是 false。
         @Ball:那如果兩個條件都是真的呢？
         @Code:orGateTT
-        @Ball:它會輸出 true，因為有一個真
-        @Ball:甚至兩個都真，都符合條件。`);
+        @Ball:它會輸出true，因為有一個真，甚至兩個都真，都符合條件。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q10
+        @Answer:true`);
 Part.Part14 = Object.freeze(`@Ball:&& 是「與」的意思，它的規則是：
         @Ball:只有兩個條件都是真的（true），結果才會是 true；
         @Ball:只要有一個是假的，結果就會是 false。
-        @Ball:比如：System.out.println(true && true); 
-        @Ball:這樣因為 true 和 true 都是真的，所以結果是 true。
-        @Ball:但如果是：System.out.println(true && false); 
+        @Code:andGateTT 
+        @Ball:這樣因為true和true都是真的，所以結果是 true。
+        @Code:andGateTF
         @Ball:這次因為其中一個是假的，所以結果是 false。
-        @Ball:再看這個：System.out.println(false && false); 
+        @Code:andGateFF
         @Ball:兩個都是假的，結果還是 false。
-        @Ball:所以 && 就是要求「全部都要是真的」才能為真。`);
+        @Ball:所以 && 就是要求「全部都要是真的」才能為真。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q11
+        @Answer:false`);
 Part.Part15 = Object.freeze(`@Ball:不只有boolean，還有其他的資料長相
-        @Ball:例如不同的數字有不同的「資料長相」。`);
-Part.Part16 = Object.freeze(`@Ball:整數類型：
+        @Ball:例如不同的數字有不同的「資料長相」。
+        @Ball:整數類型：
         @Ball:int：可以儲存大多數情況下需要的整數
-        @Ball:比如 int age = 15; 適合大部分場景。`);
+        @Ball:比如 int age = 15; 適合大部分場景。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q12
+        @Answer:1`);
 Part.Part17 = Object.freeze(`@Ball:long：如果需要儲存非常大的整數
         @Ball:比如天文數字long population = 7800000000L;
-        @Ball:記得在數字後加 L 表示它是 long。`);
+        @Ball:記得在數字後加 L 表示它是 long。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q13
+        @Answer:5`);
 Part.Part18 = Object.freeze(`@Ball:小數類型：
         @Ball:float：用來儲存小數，但精度不高
         @Ball:適合簡單運算：float price = 19.99F;
-        @Ball:記得在數字後加 F，表示它是 float。`);
+        @Ball:記得在數字後加 F，表示它是 float。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q14
+        @Answer:5.15156156456156`);
 Part.Part19 = Object.freeze(`@Ball:double：如果需要很高精度
         @Ball:比如很多位小數double pi = 3.141592653589793;
-        @Ball:double 的精度比 float 高，非常適合科學計算！`);
+        @Ball:double 的精度比 float 高，非常適合科學計算！
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q15
+        @Answer:456456456.1544454545`);
 Part.Part20 = Object.freeze(`@Ball:每種資料類型都有各自適合的場合。
         @Ball:如果我要儲存一個人的年齡，用 int 就夠了
         @Ball:但如果是天文數字，或者超級精確的小數
-        @Ball:就要用 long 或 double。`);
+        @Ball:就要用 long 或 double。
+        @Ball:「當我想要使用的數字超過int的範圍，那我要使用哪個基本資料型態?」
+        @Answer:long`);
 Part.Part21 = Object.freeze(`@Ball:還有一些比較冷門的資料長相。
-        @Ball:雖然現在用得少，但它們是程式的基石。`);
-Part.Part22 = Object.freeze(`@Ball:byte：能儲存的數字很小，因為現代電腦運算能力很強
+        @Ball:雖然現在用得少，但它們是程式的基石。
+        @Ball:byte：能儲存的數字很小，因為現代電腦運算能力很強
         @Ball:實際上會自動轉成 int，所以一般不用。
-        @Ball:例如：byte smallNumber = 100;`);
+        @Ball:例如：byte smallNumber = 100;
+        @Ball:「byte會自動轉為何種基本資料型態?」
+        @Answer:int`);
 Part.Part23 = Object.freeze(`@Ball:short：能儲存的數字很小因為現代電腦運算能力很強
         @Ball:實際上會自動轉成 int，用途和 byte 類似
-        @Ball:也很少單獨使用。short mediumNumber = 30000;`);
+        @Ball:也很少單獨使用。short mediumNumber = 30000;
+        @Ball:「short會自動轉為何種基本資料型態?」
+        @Answer:int`);
 Part.Part24 = Object.freeze(`@Ball:char：用來儲存一個字元
         @Ball:包括鍵盤上能打出來的字，或者一些特殊符號。
         @Ball:必須用單引號 ' ' 包起來
@@ -177,42 +204,63 @@ Part.Part24 = Object.freeze(`@Ball:char：用來儲存一個字元
         @Ball:它其實儲存的是字元的 編碼值（比如 ASCII 或 Unicode）
         @Ball:所以也能表示一些鍵盤上打不出的特殊字：
         @Ball:char smiley = '\\u263A';
-        @Ball:其實是Unicode 編碼，☺ 這樣就能儲存一個笑臉符號。`);
+        @Ball:其實是Unicode 編碼，☺ 這樣就能儲存一個笑臉符號。
+        @Ball:「char是什麼編碼?」
+        @Answer:Unicode`);
 Part.Part25 = Object.freeze(`@Ball:數字類型像是 int、byte、short 等
-        @Ball:也可以進行加減運算。
-        @Ball:這就和數學一樣`);
+        @Ball:也可以進行加減運算，這就和數學一樣。
+        @Ball:「boolean是數字類型嗎?」
+        @Code:q1
+        @Answer:false`);
 Part.Part26 = Object.freeze(`@Ball:電腦也能根據我們的指令進行加法和減法。
-        @Ball:比如說：int a = 0; a = a + 1; System.out.println(a); 
+        @Code:add
         @Ball:先告訴電腦要存a，a設定為0，計算a 加 1
-        @Ball:然後再把計算結果存回 a，然後就能輸出 1。`);
-Part.Part27 = Object.freeze(`@Ball:或者是減法int a = 0; a = a - 1; System.out.println(a); 
+        @Ball:然後再把計算結果存回 a，然後就能輸出 1。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q16
+        @Answer:11`);
+Part.Part27 = Object.freeze(`@Code:minus
         @Ball:先告訴電腦要存a，a設定為0，計算a 減 1
         @Ball:然後再把計算結果存回 a，然後就能輸出 -1。
-        @Ball:加法和減法很直覺`);
+        @Ball:加法和減法很直覺
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q17
+        @Answer:3`);
 Part.Part28 = Object.freeze(`@Ball:還有乘法和除法都能照著數學規則來。
-        @Ball:例如：int a = 2; a = a * 2; System.out.println(a); 
+        @Code:multiply
         @Ball:先告訴電腦要存a，a設定為2，計算a 乘2
-        @Ball:然後再把計算結果存回 a，然後就能輸出 4。`);
-Part.Part29 = Object.freeze(`@Ball:或者是除法：int a = 6; a = a / 3; System.out.println(a); 
+        @Ball:然後再把計算結果存回 a，然後就能輸出 4。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q18
+        @Answer:18`);
+Part.Part29 = Object.freeze(`@Code:divide 
         @Ball:先告訴電腦要存a，a設定為6，計算a 除以 3
         @Ball:然後再把計算結果存回 a，然後就能輸出 2。
-        @Ball:這樣我們就能輕鬆地讓電腦幫我們做數學運算了。`);
+        @Ball:這樣我們就能輕鬆地讓電腦幫我們做數學運算了。
+        @Ball:「請回答下列程式碼的輸出?」
+        @Code:q19
+        @Answer:7`);
 Part.Part30 = Object.freeze(`@Ball:我們還可以簡寫：a = a + 5; 可以寫成 a += 5;。
         @Ball:a = a - 5; 可以寫成 a -= 5;。
         @Ball:a = a * 5;可以寫成 a *= 5;。
         @Ball:a = a / 5; 可以寫成 a /= 5;。
-        @Ball:a += 1; 可以寫成a++。a -= 1; 可以寫成 a--。`);
+        @Ball:a += 1; 可以寫成a++。a -= 1; 可以寫成 a--。
+        @Ball:「a*=5和a/=9一樣嗎?」
+        @Code:q1
+        @Answer:false`);
 Part.Part31 = Object.freeze(`@Ball:class 就像生物分類裡的「屬」
         @Ball:往上可以是「科、目、門」，代表更大的分類；
-        @Ball:往下可以是「種」，代表更具體的個體。`);
-Part.Part32 = Object.freeze(`@Ball:比如：假如有個 class 是 Animal（動物）
-        @Ball:那它可能是「門」。`);
-Part.Part33 = Object.freeze(`@Ball:再有個 class 是 Mammal（哺乳類）
-        @Ball:它就可以是「綱」。`);
-Part.Part34 = Object.freeze(` @Ball:最後有個 class Dog（狗）
-        @Ball:它就到具體的「屬」或「種」了。`);
-Part.Part35 = Object.freeze(`@Ball:這樣說，class 就是一種規劃出
+        @Ball:往下可以是「種」，代表更具體的個體。
+        @Ball:比如：假如有個 class 是 Animal（動物）
+        @Ball:那它可能是「門」。
+        @Ball:再有個 class 是 Mammal（哺乳類）
+        @Ball:它就可以是「綱」。
+        @Ball:最後有個 class Dog（狗）
+        @Ball:它就到具體的「屬」或「種」了。
+        @Ball:這樣說，class 就是一種規劃出
         @Ball:「某種東西有什麼特徵」的框架！
         @Ball:但它不會馬上代表一隻具體的狗
         @Ball:而是說明了「狗應該有四隻腳、會叫，還會搖尾巴」
-        @Ball:這些基本特徵。`);
+        @Ball:這些基本特徵。
+        @Ball:「class是一個什麼?」
+        @Answer:框架`);
